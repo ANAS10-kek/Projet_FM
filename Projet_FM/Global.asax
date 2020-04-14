441cdc2900f33e226987bd57b3e6deb7d1972ed3
Anas ESSAHL <ESSAHL.Anas@ofppt-edu.ma>
2020-04-14 15:55:44 +0100

    Ajoutez des fichiers projet.

---
+++ Projet_FM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projet_FM.MvcApplication" Language="C#" %>
---
+++ Projet_FM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projet_FM.Startup))]
+namespace Projet_FM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
